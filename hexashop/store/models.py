040eb8cff3242dd4e70531e57a6be6990b974098
MrQobiljon <madrahimovq@gmail.com>
2023-07-11 21:00:08 +0500

    Product modelini yozdim

--- hexashop/store/models.py
+++ hexashop/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    choice = (
+        ('mens', "Men's"),
+        ('womens', "Women's"),
+        ('kids', "Kid's"),
+        ('accessories', "Accessories")
+    )
+    category = models.CharField(max_length=15, choices=choice)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    content = models.TextField()
+    price = models.IntegerField()
+    count = models.IntegerField(default=0)
